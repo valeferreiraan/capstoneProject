9364b49b12e406c36228c7460efb13e48f69803c
Valeria Ferreira Nocua <78361578+valeferreiraan@users.noreply.github.com>
2023-11-09 22:56:37 -0500

    creacion app en react

---
+++ react/capstone/src/App.js
@@ -0,0 +1,146 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className = "header">
+        <div className="logoUR">
+            <img src="./assets/img/thumbnail_UROSARIO_Logo_Horizontal_nuevocolor.png"/>
+        </div>
+
+        <div className="nav">
+            <a href="#">
+                URMoocs
+            </a>
+            <a>
+                Pregrados
+            </a>
+            <a>
+                Posgrados
+            </a>
+        </div>
+        <div className="search">
+            <input type="text" id="searchInput" placeholder="Buscar..."></input>
+            <button id="searchButton"><img src="./assets/img/🦆 icon _search normal 1 icon_(1).svg"></img></button>
+        </div>
+    </header>
+
+    
+  
+  <div className="content">
+    <div className="subheader">
+      <div className="Title">
+        <h1> Rutas de aprendizaje</h1>
+      </div>
+    </div>
+    <div className="expl">
+      <h2>
+        ¿Qué habilidades quieres adquirir? Te mostramos tu ruta de aprendizaje
+      </h2>
+      <br />
+      <br />
+      <br />
+      <p>
+        Utilizando procesamiento de lenguaje natural e inteligencia artificial,
+        creamos un algoritmo capaz de mostrar rutas de aprendizaje con nuestros
+        URMoocs, cursos de pregrados y cursos de posgrados...
+        <br />
+        *inserte mucho texto aquí sobre el proyecto*
+      </p>
+      <div className="boton">
+        <a href="#">Haz tu ruta de aprendizaje aquí</a>
+      </div>
+    </div>
+    <div className="catalogo">
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="fotoCard">
+          <img src="https://media.istockphoto.com/id/914314318/photo/young-man-using-laptop-with-female-student-watching-and-smiling.jpg?s=612x612&w=0&k=20&c=ky2a8mV2Fw8v4QYXFUtxAOOMs14lbOM5BPVyK1xw7-k=" />
+        </div>
+        <div className="info">
+          <h3>Inglés para todos: nivel básico</h3>
+          <p>URMooc</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
